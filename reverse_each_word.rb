b55e62847314c17c968844794f1686852bec7441
distributed-memory-9073 <noahshafer0211@gmail.com>
2020-04-03 18:23:46 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,3 +1,3 @@
 def reverse_each_word(str)
-  return str.split.collect {|e| e.reverse().split('').reverse().join}.join 
+  return str.split.collect {|e| e.reverse().split('').reverse.join}.join(' ') 
 end
